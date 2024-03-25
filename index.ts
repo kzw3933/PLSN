8b371740fa416a5efb57c9760428015c418b2780
kzw3933 <3099097649@qq.com>
2024-03-25 14:38:07 +0800

    元组类型

--- index.ts
+++ index.ts
@@ -1,22 +1,23 @@
-// 抽象类
+// 元组类型
+// 一种特殊的数组，继承了数组的方法，因此仍然可以使用push方法，编译本身不会阻止这样做
 
-// 在abstract类中使用abstract修饰的方法只能描述，不能实现
-// 抽象类无法实例化
-abstract class Vue {
-    name: string
-    constructor(name: string) {
-        this.name = name;
-    }
-    abstract init(name: string): void
+let arr1 = [1, false]
+let arr2: [number, boolean] = [1, false]
 
-}
+arr1.push(34)
+arr2.push(45)
 
+console.log(arr1)
+console.log(arr2)
 
-class VueImpl extends Vue {
-    constructor(name: string) {
-        super(name);
-    }
-    init(name: string) {
+let arr3: readonly [x: number, y?:boolean] = [1]
 
-    }
-}
+type first = typeof arr3[0]
+
+console.log(arr3['length'])
+
+let excel: [string, string, number][] = [
+    ['str1', 'str2', 23],
+    ['str3', 'str4', 34],
+    ['str5', 'str6', 45]
+]
