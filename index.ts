a50a3fa6ea0de76cfd976405e3e7701245685465
kzw3933 <3099097649@qq.com>
2024-03-25 10:09:06 +0800

    数组类型

--- index.ts
+++ index.ts
@@ -1,86 +1,30 @@
-// interface 
-// 重复定义会合并在一起
-// 定义interface对象时不能多属性也不能少属性
-// 对属性使用？可以使其变为可选
-// readonly使得属性变为只读
-// 接口可继承，支持多继承
-// interface可定义函数类型
-// 可使用索引签名以支持任意key, 避免每次定义完整填写所有属性
+// 数组类型
+
+// 普通方式
+let arr: number[] = [1,2,3]
+console.log(arr)
+
+// 泛型方式
+let arr2: Array<boolean> = [true, false, false]
 
 
+// 定义对象数组
 interface Student {
     name: string
-    age: number,
-    readonly isIkun: () => boolean
 }
 
-interface Student {
-    like?: string
+let arr3: Array<Student> = [{name: "John"}, {name: "susan"}]
+console.log(arr3)
+
+
+// 二维数组
+let arr4: number[][] = [[1,2,3],[4,5,6],[7,8,9]]
+console.log(arr4)
+let arr5: Array<Array<number>> = [[1,2,3],[4,5,6],[7,8,9]]
+console.log(arr5)
+
+
+// 数组在函数中的使用, IArguments本质是一个interface
+function sum(...args: number[]) {
+    let a: IArguments = arguments
 }
-
-interface SuperStudent extends Student {
-    id: number
-}
-
-let ikun: Student = {
-    name: 'ikun',
-    age: 18,
-    like: 'kun',
-    isIkun: () => {
-        return true
-    }
-} 
-
-let nikun: Student = {
-    name: 'nikun',
-    age: 18,
-    isIkun: () => {
-        return false
-    }
-}
-
-
-//// nikun.isIkun = () => { return true; }
-
-
-let st: SuperStudent = {
-    name: 'ikun',
-    age: 18,
-    id: 123,
-    isIkun: () => {
-        return true
-    }
-}
-
-
-interface Fn {
-    (name: string): number[]
-}
-
-const fn: Fn = function(name: string) {
-    return [1]
-}
-
-
-// 只对name, age强校验
-interface Customer {
-    name: string,
-    age: number
-    [other: string]: any
-}
-
-let customer1: Customer = {
-    name: 'ikun',
-    age: 18
-}
-
-console.log(customer1)
-
-let customer2: Customer = {
-    name: 'ikun',
-    age: 18,
-    test: "测试",
-    exam: "考试"
-}
-
-console.log(customer2)
