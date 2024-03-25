0829647154a08ba92b77df952595ba33d4f6f811
kzw3933 <3099097649@qq.com>
2024-03-25 15:23:15 +0800

    Symbol类型

--- index.ts
+++ index.ts
@@ -1,36 +1,29 @@
-// never类型
+// Symbol
 
-type A = number & string // never类型
+let a1: symbol = Symbol(1)
+let a2: symbol = Symbol(1)
 
-function error(): never {
-    throw new Error('error')
+console.log(a1 === a2)  // false
+
+// Symbol.for会去全局查找是否已经注册过这个key，如果存在就直接使用
+console.log(Symbol.for('2') === Symbol.for('2')) // true
+
+
+let obj = {
+    name: 1,
+    [a1]: 111,
+    [a2]: 222
 }
 
-function loop(): never {
-    while (true) {
-        
-    }
+console.log(obj) // 不会发生覆盖
+
+// for in 不能读取Symbol, Object.keys(obj)也无法读取
+for(let key in obj) {
+    console.log(key)
 }
 
-type B = void | number | never // void | number类型
+console.log(Object.keys(obj))
 
+console.log(Object.getOwnPropertySymbols(obj)) // 可以读取symbols，但是无法读取其他属性名了
 
-type C = 'sing' | 'dance' | 'rap'
-
-function kun(value: C) {
-    switch(value) {
-        case 'sing':
-            break
-        case 'dance':
-            break
-        case 'rap':
-            break
-        default:
-            // 兜底逻辑
-            const error: never = value
-            break
-    }
-}
-
-console.log(kun('sing'))
-
+console.log(Reflect.ownKeys(obj))
