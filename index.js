c2a1317896b065f343f6fd3781fe42225ff09c2c
kzw3933 <3099097649@qq.com>
2024-03-25 19:39:22 +0800

    tsconfig.json配置文件

---
+++ index.js
@@ -0,0 +1,10 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.b = exports.a = void 0;
+exports.a = 1;
+exports.b = 'ikun';
+var x = 1;
+var fn = function () {
+    return 123;
+};
+console.log(x, fn);
--- index.ts
+++ index.ts
@@ -1,45 +1,9 @@
-// 泛型约束
+export var a = 1;
+export var b = 'ikun'
 
-function add<T extends number>(a: T, b: T) {
-    return a + b;
+var x = 1
+var fn = function() {
+    return 123
 }
 
-console.log(add(1,2))
-
-
-
-interface Len {
-    length: number
-}
-
-function fn<T extends Len>(a: T) {
-    return a.length;
-}
-
-console.log(fn('12333'))
-
-
-let obj = {
-    name: 'foo',
-    sex: 'female'
-}
-
-// keyof约束
-type Key = keyof typeof obj
-
-function fn2<T extends object, K extends keyof T>(obj: T, key: K) {
-    return obj[key];
-}
-
-
-interface Data {
-    name: string
-    age: number
-    sex: string
-}
-
-type Options<T extends object> = {
-    [key in keyof T]?: T[key]    // 映射类型的语法 { [k in keys]: Type }
-}
-
-type B = Options<Data>
+console.log(x, fn)
