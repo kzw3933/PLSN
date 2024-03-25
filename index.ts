7e9f4623b6a1334c8e7c9a5de56050f1117bb5d6
kzw3933 <3099097649@qq.com>
2024-03-25 15:04:50 +0800

    类型推断和类型别名

--- index.ts
+++ index.ts
@@ -1,58 +1,15 @@
-// 枚举
+// 类型推论
+let arr = [1,2,3] // number[]
 
-// 数字枚举
-enum Color {
-    red = 1, // 默认从0开始
-    green,
-    blue
-}
-
-console.log(Color.red)  
-
-// 字符串枚举
-enum ColorStr {
-    red = '红色',
-    green = '绿色',
-    blue = '蓝色'
-}
-
-console.log(ColorStr.red)
-
-// 异构枚举
-enum Check {
-    yes = 1,
-    no = 'no'
-}
-
-console.log(Check.no)
-
-// 接口枚举
-interface A {
-    red: Color.red
-}
+let str   // any
+str = 456
 
 
-let obj: A = {
-    red: Color.red
-}
+// 类型别名
+type str = string
 
-// const枚举 被tsc编译后直接就是常量
-const enum Types {
-    success,
-    fail
-}
+let str1: str = 'usdiod'
 
-let code: number = 0
+type num = 1 extends number ? 1 : 0   // 和类别的层级关系有关
 
-if (code === Types.success) {
-    console.log('success')
-}
-
-// 反向映射
-enum Sexs {
-    male,
-    female
-} 
-let man: number = Sexs.male
-let key = Sexs[man]
-console.log(`value---${man}`, `key---${key}`)
+let num1: num = 1
