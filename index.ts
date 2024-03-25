d173db8b7d53ab680911db5a60e89d047ecb08e1
kzw3933 <3099097649@qq.com>
2024-03-25 15:46:36 +0800

    迭代器和生成器

--- index.ts
+++ index.ts
@@ -1,29 +1,78 @@
-// Symbol
+// 生成器
+function* gen() {
+    yield Promise.resolve('A')
+    yield 'B'
+    yield 'C'
+    yield 'D'
+}
 
-let a1: symbol = Symbol(1)
-let a2: symbol = Symbol(1)
+const genr = gen()
+console.log(genr.next())
+console.log(genr.next())
+console.log(genr.next())
+console.log(genr.next())
+console.log(genr.next())
 
-console.log(a1 === a2)  // false
+// 迭代器
 
-// Symbol.for会去全局查找是否已经注册过这个key，如果存在就直接使用
-console.log(Symbol.for('2') === Symbol.for('2')) // true
+let set: Set<number> = new Set([1,2,3,1,2,3]) // 去重后1,2,3
+let map: Map<any, any> = new Map<number, number>
+
+let Arr = [1,2,3]
+map.set(Arr, '数组123')
+
+console.log(map.get(Arr))
+
+const each = (value: any) => {
+    let iter: any = value[Symbol.iterator]()
+    let next: any = {done: false}
+    while(!next.done) {
+        next = iter.next()
+        if(!next.done) {
+            console.log(next.value)
+        }
+    }
+}
+
+each(map)
 
 
+// 迭代器的语法糖, for of不能用于对象，对象上没有iterator
+for(let val of set) {
+    console.log(val)
+}
+
+// 解构的底层原理也是调用iterator
+let [a, b, c] = [1,2,3]
+let copy = [...Arr]
+console.log(a, b, c)
+console.log(copy)
+
+// 对象支持for of
 let obj = {
-    name: 1,
-    [a1]: 111,
-    [a2]: 222
+    max: 5,
+    current: 0,
+    [Symbol.iterator]() {
+        return {
+            max: this.max,
+            current: this.current,
+            next() {
+                if(this.current == this.max) {
+                    return {
+                        value: undefined,
+                        done: true
+                    }
+                } else {
+                    return {
+                        value: this.current++,
+                        done: false
+                    }
+                }
+            }
+        }
+    }
 }
 
-console.log(obj) // 不会发生覆盖
-
-// for in 不能读取Symbol, Object.keys(obj)也无法读取
-for(let key in obj) {
-    console.log(key)
+for(let val of obj) {
+    console.log(val)
 }
-
-console.log(Object.keys(obj))
-
-console.log(Object.getOwnPropertySymbols(obj)) // 可以读取symbols，但是无法读取其他属性名了
-
-console.log(Reflect.ownKeys(obj))
