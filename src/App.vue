151f3b721ece5c3372e07ad4368890470ca9ac2b
kzw3933 <3099097649@qq.com>
2024-03-27 15:09:19 +0800

    模板语法、vue指令

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,72 @@
-<script setup lang="ts">
-import HelloWorld from './components/HelloWorld.vue'
-</script>
-
 <template>
   <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
+    <div>
+      <!-- 模板插值 -->
+      {{ num }}
+      {{ arr.map(v => ({num: v})) }}
+    </div>
+    <div>
+      <!-- vue指令 -->
+      <div v-text="str"></div>
+      <div v-html="html"></div>
+      <div v-if="bool">
+        true
+      </div>
+      <div v-else>
+        false
+      </div>
+      <div v-show="bool">
+        content
+      </div>
+      <button v-on:click="onclick">点击</button>
+      <div @click="parent">
+        <button v-on:[event].stop="onclick">点击</button> <!--  阻止冒泡 -->
+      </div>
+      <div v-bind:id="id">
+        {{ data }}
+      </div>
+      <div>
+        <input v-model="data" type="text">
+      </div>
+    </div>
+    <div v-for="item in strarr">
+      {{ item }}
+    </div>
+    <div v-for="(item, index) in strarr">
+      {{ item }} -- {{ index }}
+    </div>
+
+    <div :key="index" v-for="(item, index) in strarr">
+      {{ item }} -- {{ index }}
+    </div>
+    <div v-once>
+      {{ data }}
+    </div>
   </div>
-  <HelloWorld msg="Vite + Vue" />
 </template>
 
-<style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: filter 300ms;
-}
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
-}
+<script setup lang="ts">
+  import { ref } from 'vue';
+
+  let num: number = 0;
+  const arr: number[] = [1,2,3]
+  const str: string = 'asdfghjkl'
+  const html: string = "<span style='color:red'>asdfghjkl</span>"
+  let bool: boolean = false
+  const onclick = () => {
+    num += 1;
+    console.log('点击'+num+'次')
+  }
+  const event: string = 'click'
+  const parent = () => {
+    console.log('父组件点击')
+  }
+  let id = 'start'
+  let data = ref('kzw')  // 使用ref才能成为响应式
+
+  const strarr: string[] = ['tyyty', 'sjkjds', 'sjdkksd']
+</script>
+
+<style>
+
 </style>
